9c7e94e41272bd6fac3546a7db2af09a0f017ab6
Ewerton Ramos <ewerton.ramos@gmail.com>
2020-09-05 21:17:13 +0200

    initial code

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            {name: "Name1"},
-            {name: "Joao"},
-            {name: "Jose"}
+            {name: "Name1", age: 20},
+            {name: "Joao", age: 40},
+            {name: "Jose", age: 30}
         ]
     }
 
@@ -31,9 +31,9 @@ class App extends Component {
                 <h1>Hello world!</h1>
                 <p>This is working</p>
                 <button onClick={this.switchNameHandler.bind(this, 'BOUND')}>Changen name</button>
-                <Person click={this.switchNameHandler.bind(this, 'BINDING')} personName={this.state.persons[0].name}> person info </Person>
-                <Person change={this.nameChangeHandler} personName={this.state.persons[1].name}> another info </Person>
-                <Person click={this.switchNameHandler} personName={this.state.persons[2].name}/>
+                <Person click={this.switchNameHandler.bind(this, 'BINDING')} personName={this.state.persons[0].name} age={this.state.persons[0].age}> Some info here </Person>
+                <Person change={this.nameChangeHandler} personName={this.state.persons[1].name} age={this.state.persons[1].age}> Another info </Person>
+                <Person click={this.switchNameHandler} personName={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,8 @@ import './Person.css'
 const person = (props) => {
     return (
         <div className="Person">
-            <p onClick={props.click}>I'm {props.personName}! numer = {Math.ceil(Math.random() * 20)}</p>
+            <p onClick={props.click}>My name is {props.personName}, I am {props.age} years old,
+                with a random number = {Math.ceil(Math.random() * 20)}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.personName}/>
         </div>
